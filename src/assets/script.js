93f90ec313181ca010da79062eff3f4e822ecd9a
Stefan <git@stefn.co.uk>
2025-01-21 02:15:48 +0000

    Nicer CSS

--- src/assets/script.js
+++ src/assets/script.js
@@ -1,10 +1,17 @@
 (() => {
 	function masonry() {
-		var elem = document.querySelector(".homepage-images");
+		const width = window.screen.width;
+		let gutter = 10;
+		if (width > 1000) {
+			gutter = 30;
+		} else if (width > 798) {
+			gutter = 20;
+		}
+		const elem = document.querySelector(".homepage-images");
 		new Masonry(elem, {
 			itemSelector: "picture",
 			columnWidth: 200,
-			gutter: 20,
+			gutter: gutter,
 		});
 	}
 
