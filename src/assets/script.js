3375a59169e815c058079c9a12283a831b72e7ad
Stefan <git@stefn.co.uk>
2025-01-22 00:13:19 +0000

    DIY Masonry

--- src/assets/script.js
+++ src/assets/script.js
@@ -1,31 +1,53 @@
+"use strict";
+
 (() => {
-	let masonryObject;
+	let container;
+	let images;
 
-	function masonry() {
-		masonryObject?.destroy();
+	function init() {
+		container = document.querySelector(".homepage-images");
+		if (!container) return;
+		const imageLinks = Array.from(container.querySelectorAll("a"));
+		imageLinks.forEach((link) => link.remove());
+		images = imageLinks;
+	}
 
-		const width = window.screen.width;
-		let gutter = 10;
-		let columnWidth = 200;
+	function addColumn(els) {
+		const column = document.createElement("div");
+		column.classList.add("column");
+		els.forEach((el) => column.appendChild(el));
+		container.appendChild(column);
+	}
 
-		if (width > 1000) {
-			gutter = 30;
-		} else if (width > 660) {
-			gutter = 20;
+	function createMasonryLayout() {
+		if (!container) return;
+		container.classList.remove("hidden");
+
+		const containerWidth = container.offsetWidth;
+		const isMobile = window.innerWidth < 400;
+
+		container.innerHTML = "";
+
+		if (isMobile) {
+			addColumn(images);
 		} else {
-			return;
-		}
+			// masonry layout for larger screens
+			const columnCount = Math.floor(containerWidth / 200);
+			const columns = Array.from({ length: columnCount }, () => []);
 
-		const elem = document.querySelector(".homepage-images");
-		masonryObject = new Masonry(elem, {
-			itemSelector: "a",
-			columnWidth: columnWidth,
-			gutter: gutter,
-		});
+			images.forEach((image, index) => {
+				columns[index % columnCount].push(image);
+			});
+
+			columns.forEach((column) => {
+				addColumn(column);
+			});
+		}
 	}
 
 	addEventListener("DOMContentLoaded", () => {
-		masonry();
-		addEventListener("resize", masonry, false, false);
+		init();
+		createMasonryLayout();
+		addEventListener("resize", createMasonryLayout, false, false);
 	});
 })();
